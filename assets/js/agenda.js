c6c204217903e0df6d18d6027904f6540a914fd1
Adila <219802882+a-adila@users.noreply.github.com>
2026-06-18 05:19:57 +0200

    feat(css, js): add css style link to index.html, rewrite agenda script for cards generation, add favicon, style agenda.html

--- assets/js/agenda.js
+++ assets/js/agenda.js
@@ -1,53 +1,69 @@
+const windowWidth = window.innerWidth;
+
 const response = await fetch("/assets/spectacles.json");
 const data = await response.json();
 const shows = data.spectacles;
 
-// I get the html element
-const showsContainer = document.querySelector(".agenda-grid");
+const cardsContainer = document.querySelector(".agenda-grid");
 
+// Index to track where to create a new bootstrap row
 let index = 0;
 let currentRow;
 
-// A card = img (img) + schedule (p) + artist name (h3) + show name (p)
 shows.forEach((show) => {
+  // Create a card
   const card = document.createElement("div");
-  card.classList.add("show-card");
+  card.classList.add("show__card");
+  // Create elements of a card and set classes if needed
+  const showPictureWrapper = document.createElement("div");
+  showPictureWrapper.classList.add("show__picture");
+  const showImage = document.createElement("img");
+  showImage.src = show.image;
+  showImage.alt = show.titre;
 
-  card.style.backgroundImage = `url(${show.image})`;
+  const showArtist = document.createElement("h3");
+  showArtist.textContent = show.artiste;
+  showArtist.classList.add("show__artist");
 
-  const schedule = document.createElement("div");
+  const showLink = document.createElement("a");
+  showLink.textContent = show.titre;
 
-  const date = document.createElement("p");
-  date.classList.add("show-date");
-  date.textContent = show.date;
-  const separator = document.createTextNode(" • ");
-  const hour = document.createElement("p");
-  hour.textContent = show.horaire;
-  hour.classList.add("show-hour");
+  const fullProgressionBar = document.createElement("div");
+  fullProgressionBar.classList.add("show__full-progression-bar");
+  const currentProgressionBar = document.createElement("div");
+  currentProgressionBar.classList.add("show__current-progression-bar");
 
-  const artistShowInfo = document.createElement("div");
-  artistShowInfo.classList.add("artistShowInfo");
-  const artist = document.createElement("h3");
-  artist.textContent = show.artiste;
-  artist.classList.add("show-artist");
-  const showName = document.createElement("h4");
-  showName.textContent = show.titre;
-  showName.classList.add("show-name");
+  // Compute current available slots
 
-  schedule.appendChild(date);
-  schedule.appendChild(separator);
-  schedule.appendChild(hour);
-  artistShowInfo.appendChild(artist);
-  artistShowInfo.appendChild(showName);
+  const pourcentage = (show.places_vendues / show.places_total) * 100;
 
-  card.appendChild(schedule);
-  card.appendChild(artistShowInfo);
+  // If there are no more tickets available
+  if (pourcentage === 100) {
+    const showSoldOut = document.createElement("p");
+    showSoldOut.classList.add("show__label", "show__label--full");
+    showSoldOut.textContent = "Sold out";
+    showPictureWrapper.appendChild(showSoldOut);
+  }
+  fullProgressionBar.appendChild(currentProgressionBar);
 
-  showsContainer.appendChild(card);
+  // Link everything together
+
+  showPictureWrapper.appendChild(showImage);
+  showPictureWrapper.appendChild(showLink);
+
+  card.appendChild(showPictureWrapper);
+  card.appendChild(showArtist);
+  card.appendChild(showLink);
+  card.appendChild(fullProgressionBar);
+
+  const progressBar = card.querySelector(".show__current-progression-bar");
+  progressBar.style.width = pourcentage + "%";
 
   if (index % 3 === 0) {
     const newRow = document.createElement("div");
-    newRow.classList.add("row, px-0");
+    newRow.classList.add("row");
+    newRow.classList.add("gx-5");
+    newRow.classList.add("px-0");
 
     const newCol = document.createElement("div");
     newCol.classList.add("col-md-4");
@@ -55,14 +71,15 @@ shows.forEach((show) => {
     newCol.appendChild(card);
     newRow.appendChild(newCol);
 
-    showsContainer.appendChild(newRow);
+    cardsContainer.appendChild(newRow);
 
+    index++;
     currentRow = newRow;
   } else {
     const newCol = document.createElement("div");
     newCol.classList.add("col-md-4");
     newCol.appendChild(card);
     currentRow.appendChild(newCol);
+    index++;
   }
-  index++;
 });
--- assets/js/header.js
+++ assets/js/header.js
@@ -1,4 +1,23 @@
-const response = await fetch("../../assets/components/header.html");
-const htmlHeader = await response.text();
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/assets/components/header.html")
+    .then((r) => r.text())
+    .then((html) => {
+      document.getElementById("header").innerHTML = html;
+      const currentPage = window.location.pathname.split("/").pop();
 
-document.getElementById("header").innerHTML = htmlHeader;
+      document.querySelectorAll(".header__navbar-link").forEach((link) => {
+        const linkPage = link.getAttribute("href").split("/").pop();
+        if (linkPage === currentPage) {
+          link.classList.add("active");
+        } else {
+          link.classList.remove("active");
+        }
+      });
+    });
+
+  fetch("/assets/components/footer.html")
+    .then((r) => r.text())
+    .then((html) => {
+      document.getElementById("footer").innerHTML = html;
+    });
+});
