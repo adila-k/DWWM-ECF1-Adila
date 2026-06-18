ae273c5c50190b1ff683440204756c3c15c416ef
Adila <219802882+a-adila@users.noreply.github.com>
2026-06-18 10:13:47 +0200

    feat(js): add burger menu to header mobile versio

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,15 +3,29 @@ document.addEventListener("DOMContentLoaded", function () {
     .then((r) => r.text())
     .then((html) => {
       document.getElementById("header").innerHTML = html;
+
+      // --- Tout ce qui dépend du contenu du header va ICI ---
+
       const currentPage = window.location.pathname.split("/").pop();
 
-      document.querySelectorAll(".header__navbar-link").forEach((link) => {
+      document.querySelectorAll(".header__navbar-link a").forEach((link) => {
         const linkPage = link.getAttribute("href").split("/").pop();
         if (linkPage === currentPage) {
           link.classList.add("active");
         } else {
           link.classList.remove("active");
         }
+      });
+
+      //////////////////
+      //  BURGER MENU //
+      //////////////////
+
+      const burgerButton = document.querySelector(".header__burger-menu");
+      const navbar = document.querySelector(".header__navbar");
+
+      burgerButton.addEventListener("click", () => {
+        navbar.classList.toggle("is-open");
       });
     });
 
